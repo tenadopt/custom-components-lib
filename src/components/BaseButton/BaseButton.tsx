c052d0ae76b24cff7f7c07a135dba36fe3d9cd1c
Tenadopt <yury.ignatiev@gmail.com>
2024-05-16 15:41:44 +0300

    fix: styles, gitignore

--- src/components/BaseButton/BaseButton.tsx
+++ src/components/BaseButton/BaseButton.tsx
@@ -1,24 +1,19 @@
-import React, { FC, ComponentProps } from 'react';
+import React, { ComponentProps } from 'react';
 import styles from '../BaseButton/BaseButton.module.scss';
 import cnBind from 'classnames/bind';
 
 export interface BaseButtonProps extends ComponentProps<'button'> {
+    /** Button variant */
     variant?: 'text' | 'contained' | 'outlined';
+    /** Button size */
     size?: 'small' | 'medium' | 'large';
 }
 
 const cx = cnBind.bind(styles);
 
-const BaseButton: FC<BaseButtonProps> = ({ variant, size, ...props }) => {
-    const btn = cx('button', props.className, {
-        disabled: props.disabled,
-        small: size === 'small',
-        medium: size === 'medium',
-        large: size === 'large',
-        text: variant === 'text',
-        outlined: variant === 'outlined',
-        contained: variant === 'contained',
-    });
+/** Custom Button Component */
+const BaseButton = ({ variant, size, ...props }): BaseButtonProps => {
+    const btn = cx('button', props.className, variant, size);
 
     return (
         <button
