92c620ad3dc40fb556ab76d337bf322317c4e6a4
Tenadopt <yury.ignatiev@gmail.com>
2024-05-16 14:05:39 +0300

    fix: add classNames library

--- src/components/BaseButton/BaseButton.tsx
+++ src/components/BaseButton/BaseButton.tsx
@@ -1,20 +1,33 @@
 import React, { FC, ComponentProps } from 'react';
 import styles from '../BaseButton/BaseButton.module.scss';
+import cnBind from 'classnames/bind';
 
 export interface BaseButtonProps extends ComponentProps<'button'> {
     variant?: 'text' | 'contained' | 'outlined';
-    color?: string;
     size?: 'small' | 'medium' | 'large';
 }
 
-const BaseButton: FC<BaseButtonProps> = ({ variant, color, size, ...props }) => {
-    const sizeClass = size ? styles[size] : '';
-    const variantClass = variant ? styles[variant] : '';
-    const disabledClass = props.disabled ? styles.disabled : '';
-    const className = `${styles.button} ${sizeClass} ${variantClass} ${disabledClass}`;
+const cx = cnBind.bind(styles);
+
+const BaseButton: FC<BaseButtonProps> = ({ variant, size, ...props }) => {
+    const btn = cx('button', props.className, {
+        disabled: props.disabled,
+        small: size === 'small',
+        medium: size === 'medium',
+        large: size === 'large',
+        text: variant === 'text',
+        outlined: variant === 'outlined',
+        contained: variant === 'contained',
+    });
 
     return (
-        <button type="button" className={className} style={{ color }} {...props}>
+        <button
+            type="button"
+            className={btn}
+            disabled={props.disabled}
+            onClick={props.onClick}
+            {...props}
+        >
             {props.children}
         </button>
     );
